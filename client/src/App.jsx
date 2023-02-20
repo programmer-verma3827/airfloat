53c3305b96a39b6b1429b744aaa67bd6111255f2
Pranav Verma <vermapranav3827@gmail.com>
2023-02-20 14:22:30 +0530

    new commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+    <header className="p-4 flex justify-between">
+    <a href="" className="flex items-center gap-1">
+    <svg xmlns="http://www.w3.org/2000/svg" width="25" viewBox="0 0 27 27">
+    <path fill="var(--white)" d="M5.41 16H18a2 2 0 0 0 2-2 1 1 0 0 1 2 0 4 4 0 0 1-4 4H5.41l2.3 2.3a1 1 0 0 1-1.42 1.4l-4-4a1 1 0 0 1 0-1.4l4-4a1 1 0 1 1 1.42 1.4L5.4 16zM6 8a2 2 0 0 0-2 2 1 1 0 0 1-2 0 4 4 0 0 1 4-4h12.59l-2.3-2.3a1 1 0 1 1 1.42-1.4l4 4a1 1 0 0 1 0 1.4l-4 4a1 1 0 0 1-1.42-1.4L18.6 8H6z" class="heroicon-ui">
+    </path>
+    </svg>
+    <span className="font-bold text-xl">AirFloat</span>
+    </a>
+    <div className="flex">
+    <div>Anywhere</div>
+    <div>Any week</div>
+    <div>Add guests</div>
+    <button>
+    <svg xmlns="http://www.w3.org/2000/svg" width="25" viewBox="0 0 27 27">
+    <path fill="var(--white)" d="M5.41 16H18a2 2 0 0 0 2-2 1 1 0 0 1 2 0 4 4 0 0 1-4 4H5.41l2.3 2.3a1 1 0 0 1-1.42 1.4l-4-4a1 1 0 0 1 0-1.4l4-4a1 1 0 1 1 1.42 1.4L5.4 16zM6 8a2 2 0 0 0-2 2 1 1 0 0 1-2 0 4 4 0 0 1 4-4h12.59l-2.3-2.3a1 1 0 1 1 1.42-1.4l4 4a1 1 0 0 1 0 1.4l-4 4a1 1 0 0 1-1.42-1.4L18.6 8H6z" class="heroicon-ui">
+    </path>
+    </svg>
+    </button>
+    </div>
+    </header>
     </div>
   )
 }
